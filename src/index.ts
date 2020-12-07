3a735b144533f303cd3a0c20e51eac318a63dc29
ivannsilvero <ivannsilvero@gmail.com>
2020-12-07 14:05:07 -0300

    CORS enabled

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import 'reflect-metadata';
 import express from 'express';
+import cors from 'cors';
 import { createConnection } from 'typeorm';
 
 import { app as IndexRouter } from './routes/index.routes';
@@ -26,6 +27,7 @@ const main = async () => {
 
     app.use(express.json());
     app.use(express.urlencoded({ extended: false }));
+    app.use(cors());
 
     app.use(IndexRouter);
 
